2aee311b1e104c662a5152a5df7058beee1b61a7
waxpoo <muhammadruhulbayan.mrb@gmail.com>
2025-04-21 08:48:20 +0700

    baru

---
+++ app/Http/Controllers/RoomControllerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoomController;
+use Illuminate\Http\Request;
+
+class RoomControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoomController $roomController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoomController $roomController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RoomController $roomController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoomController $roomController)
+    {
+        //
+    }
+}
